8dab47cedd172dfcca4fb415e3ec447ece472389
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-09-28 02:40:51 -0300

    Atualização dos cards de exibição dos jogadores cadastrados

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -38,7 +38,7 @@
       @foreach($player as $players)
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">{{$players->name}}</text></svg>
            <div class="card-body">
 
            <p class="card-text">
@@ -74,7 +74,7 @@
   </div>
 
 </main>
-      
+<div class="d-flex align-items-center justify-content-center">
 {{ $player->links()}}
-
+</div>
 @endsection
