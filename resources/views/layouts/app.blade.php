e57ecd882f538af8977e6bd1b70b73ea00c132a7
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-10-08 21:54:17 -0300

    Criação do footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,6 +101,17 @@
         <main class="py-4">
             @yield('content')
         </main>
+        <!-- Footer -->
+<footer class="page-footer font-small blue">
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2020 Copyright:
+  <a href="https://mdbootstrap.com/"> mgbs@discente.ifpe.edu.br</a>
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
     </div>
 </body>
 </html>
